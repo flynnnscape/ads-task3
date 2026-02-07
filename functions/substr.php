1bc1e95a40b49ed88532d32a391cb6871dfbc049
flynnn <lorensbaldon@gmail.com>
2026-02-07 21:31:52 +0800

    Adding functions

---
+++ functions/substr.php
@@ -0,0 +1,30 @@
+<?php
+    include "../db.php";
+
+    $title = "SUBSTR()";
+    $sql = "SELECT SUBSTR('W3Schools', 3, 4) AS value;";
+    $result = mysqli_query($conn, $sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <h2><?php echo $title; ?></h2>
+    <a href="../index.php">Back</a>
+    <pre><?php echo htmlspecialchars($sql); ?></pre>
+    <table border="1" cellpadding="5" cellspacing="0">
+        <tr>
+            <th>Result</th>
+        </tr>
+        <?php while($row = mysqli_fetch_assoc($result)): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($row['value']); ?></td>
+            </tr>
+        <?php endwhile; ?>
+    </table>    
+</body>
+</html>
